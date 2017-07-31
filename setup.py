8b21d37e5ec345fd1b8d16bef768ecd2966fccdf
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2017-07-31 11:54:43 +0500

    Bump val version.

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ def load_requirements(*requirements_paths):
 
 setup(
     name='edxval',
-    version='0.1.0',
+    version='0.1.1',
     author='edX',
     url='http://github.com/edx/edx-val',
     description='edx-val',
