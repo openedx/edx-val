4ad6348f5e2523f6515f2addcbdda7069f0f1686
muzaffaryousaf <muzaffar.yousaf@arbisoft.com>
2017-10-26 13:02:44 +0500

    Remove subtitles code from VAL.
    EDU-1369

---
+++ edxval/migrations/0008_auto_20171026_0359.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('edxval', '0007_transcript_credentials_state'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='subtitle',
+            name='video',
+        ),
+        migrations.DeleteModel(
+            name='Subtitle',
+        ),
+    ]
