d7cdffc7230d09d6a71f24e0f9472c3660011a5e
noraiz-anwar <noraizbhatti@gmail.com>
2018-12-12 15:03:50 +0500

    fix transcript download failure

---
+++ edxval/tests/test_models.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from django.test import TestCase
+
+from edxval.models import Video, VideoTranscript
+from edxval.tests import constants
+
+
+class VideoTranscriptTest(TestCase):
+    """
+    Test VideoTranscript model
+    """
+
+    def setUp(self):
+        """
+        Creates Profile objects and a video object
+        """
+        self.transcript_data = constants.VIDEO_TRANSCRIPT_CIELO24
+        super(VideoTranscriptTest, self).setUp()
+    
+    def test_filename_property_new_line(self):
+        """
+        Test that filename does not contain any "\n". 
+        New line is not permmited in response headers.
+        """
+        video = Video.objects.create(**constants.VIDEO_DICT_NEW_LINE)
+        video_trancript = VideoTranscript.objects.create(
+            video=video,
+            language_code=self.transcript_data['language_code'],
+            file_format=self.transcript_data['file_format'],
+            provider=self.transcript_data['provider'],
+        )
+
+        self.assertFalse('\n' in video_trancript.filename)
