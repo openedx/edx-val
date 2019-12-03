6cdcedf8ef6a0519987fb2c4d69674f835720a92
Brian Wilson <brian@edx.org>
2019-12-03 01:03:38 -0500

    Update version number.

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ def load_requirements(*requirements_paths):
     return list(requirements)
 
 
-VERSION = '1.1.30'
+VERSION = '1.1.31'
 
 if sys.argv[-1] == 'tag':
     print("Tagging the version on github:")
