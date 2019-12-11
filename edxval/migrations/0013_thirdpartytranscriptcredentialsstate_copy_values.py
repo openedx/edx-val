424f143eecc4109674a65f19a185ba21a4da2b04
brianhw <brian@edx.org>
2019-12-11 12:11:33 -0500

    Add migration: copy 'exists' values to 'has_creds' (#195)
    Change type of 'exists' to be nullable.
    
    Update version to 1.1.33.

---
+++ edxval/migrations/0013_thirdpartytranscriptcredentialsstate_copy_values.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+def copy_column_values(apps, schema_editor):
+    """
+    Copy the 'exists' field's value into the 'has_creds' field.
+    """
+    ThirdPartyTranscriptCredentialsState = apps.get_model('edxval', 'ThirdPartyTranscriptCredentialsState')
+    for credentials_state in ThirdPartyTranscriptCredentialsState.objects.all():
+        credentials_state.has_creds = credentials_state.exists
+        credentials_state.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('edxval', '0012_thirdpartytranscriptcredentialsstate_has_creds'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            copy_column_values,
+            reverse_code=migrations.RunPython.noop,  # Allow reverse migrations, but make it a no-op.
+        ),
+    ]
