f831be495d4939aa10ff2c107659a540bea62daf
DawoudSheraz <dawoud.sheraz@gmail.com>
2021-04-16 19:01:56 +0500

    docs: add readme for mgt command usage

--- edxval/settings/test.py
+++ edxval/settings/test.py
@@ -28,25 +28,5 @@ PROVIDER_STATES_URL = '{}/edxval/pact/provider_states/'.format(PROVIDER_BASE_URL
 PACT_BROKER_BASE_URL = 'http://localhost:9292'
 PUBLISH_VERIFICATION_RESULTS = True
 
-JWT_AUTH = {
-    'JWT_AUDIENCE': 'test-aud',
-    'JWT_DECODE_HANDLER': 'edx_rest_framework_extensions.auth.jwt.decoder.jwt_decode_handler',
-    'JWT_ISSUER': 'test-iss',
-    'JWT_LEEWAY': 1,
-    'JWT_SECRET_KEY': 'test-key',
-    'JWT_ALGORITHM': 'HS256',
-    'JWT_SUPPORTED_VERSION': '1.0.0',
-    'JWT_VERIFY_AUDIENCE': True,
-    'JWT_VERIFY_EXPIRATION': True,
-    'JWT_AUTH_HEADER_PREFIX': 'JWT',
-    'JWT_ISSUERS': [
-        {
-            'ISSUER': 'test-issuer-1',
-            'SECRET_KEY': 'test-secret-key',
-            'AUDIENCE': 'test-audience',
-        }
-    ],
-}
 
-MIDDLEWARE = list(MIDDLEWARE) + ['edxval.pacts.middleware.AuthenticationMiddleware']
-MIDDLEWARE = tuple(MIDDLEWARE)
+MIDDLEWARE = MIDDLEWARE + ('edxval.pacts.middleware.AuthenticationMiddleware',)
